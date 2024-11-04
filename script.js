03b1c2c9825bd9ab747fee2ede72d487b795e531
Thelonelyship <allanbram@hotmail.com>
2024-11-04 11:07:20 +0000

    Git Broke so Testing

---
+++ script.js
@@ -0,0 +1,10 @@
+// ! Hamburger Menu
+
+function toggleMenu(){
+    const hamburger = document.querySelector(".hamburgermenu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    hamburger.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+
